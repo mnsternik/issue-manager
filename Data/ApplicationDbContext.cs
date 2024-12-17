2ff6df707b88b7348b532aef154e4b2fbec87007
mnsternik <mnsternik@gmail.com>
2024-12-17 04:34:51 +0100

    New domain models added

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using IssueManager.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace IssueManager.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
     }
 }
