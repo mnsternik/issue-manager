bfc53aee7d5c70cab030fdacab8b929583a4e85c
mnsternik <mnsternik@gmail.com>
2024-12-19 04:08:36 +0100

    Seed data added

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,6 +12,37 @@ namespace IssueManager.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Relacja do użytkownika (AssignedToUser)
+            modelBuilder.Entity<Request>()
+                .HasOne(r => r.AssignedToUser)
+                .WithMany()
+                .HasForeignKey(r => r.AssignedToUserId)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            // Relacja do zespołu (AssignedToTeam)
+            modelBuilder.Entity<Request>()
+                .HasOne(r => r.AssignedToTeam)
+                .WithMany()
+                .HasForeignKey(r => r.AssignedToTeamId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Team>().HasData(new Team
+            {
+                Id = 1,
+                Name = "Default Team"
+            });
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                Id = 1,
+                Name = "Default Category"
+            });
+        }
+
         public DbSet<Request> Requests { get; set; }
         public DbSet<Team> Teams { get; set; }
         public DbSet<Category> Categories { get; set; }
