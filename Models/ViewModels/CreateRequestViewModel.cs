e0c7f22b24b5895a6f035950d0fd0b51a43ef8f9
mnsternik <mnsternik@gmail.com>
2024-12-20 02:08:49 +0100

    CreateRequestViewModel created

---
+++ Models/ViewModels/CreateRequestViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace IssueManager.Models.ViewModels
+{
+    public class CreateRequestViewModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        public RequestPriority Priority { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Attachment>? Attachments { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int SelectedCategoryId { get; set; }
+
+        [Display(Name = "Target user")]
+        public string? SelectedUserId { get; set; }
+
+        [Display(Name = "Target team")]
+        public int? SelectedTeamId { get; set; }
+    }
+}
