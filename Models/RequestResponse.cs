d018c256cde428a7f89a57489ea02676b2393955
mnsternik <mnsternik@gmail.com>
2025-01-15 23:33:18 +0100

    RequestResponse created

---
+++ Models/RequestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueManager.Models
+{
+    public class RequestResponse
+    {
+        public int Id { get; set; }
+        public string AuthorId { get; set; }
+        public int RequestId { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        [Required]
+        public string ResponseText { get; set; }
+
+        public User Author { get; set; }
+        public Request Request { get; set; }
+    }
+}
