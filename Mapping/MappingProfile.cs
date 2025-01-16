92a315dfc5887f8dbd86a56b068b496afd6d3d99
mnsternik <mnsternik@gmail.com>
2025-01-16 23:50:36 +0100

    MappingProfile created

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using IssueManager.Models;
+using IssueManager.Models.ViewModels;
+
+namespace IssueManager.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<CreateRequestViewModel, Request>();
+            CreateMap<EditRequestViewModel, Request>(); 
+            CreateMap<Request, DetailsRequestViewModel>();
+            CreateMap<Request, EditRequestViewModel>();
+            CreateMap<Request, RequestsListItemViewModel>();
+            //CreateMap<RequestResponseViewModel, RequestResponse>();
+            CreateMap<RequestResponse, RequestResponseViewModel>();
+        }
+    }
+}
