af34c355018340c27314b9ac8c6aee74d859a07f
mnsternik <mnsternik@gmail.com>
2025-01-31 11:42:40 +0100

    Seeding roles data added

--- Program.cs
+++ Program.cs
@@ -1,59 +1,82 @@
 using IssueManager.Data;
 using IssueManager.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using IssueManager.Mapping;
+using IssueManager.Utilities;
 
 namespace IssueManager
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                                   ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<User>(options =>
             {
-                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+                options.SignIn.RequireConfirmedAccount = false; 
+            })
+                .AddRoles<IdentityRole>() 
+                .AddEntityFrameworkStores<ApplicationDbContext>(); 
 
-                builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlServer(connectionString));
+            builder.Services.AddControllersWithViews();
 
-                builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-                builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
-                    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-                builder.Services.AddControllersWithViews();
-            }
+            builder.Services.AddAutoMapper(typeof(MappingProfile));
+            builder.Services.AddTransient<DataSeeder>(); 
 
             var app = builder.Build();
+
+            // Creating roles and admin user
+            using (var scope = app.Services.CreateScope())
             {
-                // Configure the HTTP request pipeline.
-                if (app.Environment.IsDevelopment())
+                var services = scope.ServiceProvider;
+                try
                 {
-                    app.UseMigrationsEndPoint();
+                    var dataSeeder = services.GetRequiredService<DataSeeder>();
+
+                    await dataSeeder.CreateRoles();
+                    await dataSeeder.CreateAdminUser();
                 }
-                else
+                catch (Exception ex)
                 {
-                    app.UseExceptionHandler("/Home/Error");
-                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                    app.UseHsts();
+                    Console.WriteLine($"Error creating roles or admin user: {ex.Message}, {ex.InnerException}");
                 }
-
-                app.UseHttpsRedirection();
-                app.UseStaticFiles();
-
-                app.UseRouting();
-
-                app.UseAuthentication();
-
-                app.UseAuthorization();
-
-                app.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-                app.MapRazorPages();
-
-                app.Run();
             }
-        }
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts(); 
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Requests}/{action=Index}/{id?}");
+
+            app.MapRazorPages();
+
+            app.Run();
+        }   
     }
 }
