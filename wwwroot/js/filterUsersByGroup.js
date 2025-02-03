a00d124cc5e19ccfc730ec140497843f9f8f6052
mnsternik <mnsternik@gmail.com>
2025-02-03 20:11:59 +0100

    Bug fixed

--- wwwroot/js/filterUsersByGroup.js
+++ wwwroot/js/filterUsersByGroup.js
@@ -22,6 +22,23 @@
     assignedTeamSelect.addEventListener("change", function() {
         const selectedTeamId = this.value; 
 
+        // if no value is selected, show all users
+        console.log(selectedTeamId == false); 
+        if (!selectedTeamId) {
+            assignedUserSelect.innerHTML = '<option value="">-- Select --</option>'; // Reset options
+
+            for (const team in usersByTeam) {
+                console.log(team);
+                usersByTeam[team].forEach(user => {
+                    const option = document.createElement("option");
+                    option.value = user.id;
+                    option.textContent = user.name;
+                    assignedUserSelect.appendChild(option);
+                })
+            }
+            return;
+        }
+
         assignedUserSelect.innerHTML = '<option value="">-- Select --</option>'; // Reset options
 
         if (usersByTeam[selectedTeamId]) {
@@ -33,5 +50,4 @@
             })
         }
     })
-
 })
