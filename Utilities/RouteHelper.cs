e86fec4ff7f88bbb704bb6d376896ff5859b8d95
mnsternik <mnsternik@gmail.com>
2025-02-10 23:07:38 +0100

    RouteHelper class created

---
+++ Utilities/RouteHelper.cs
@@ -0,0 +1,23 @@
+﻿using IssueManager.Models.ViewModels.Requests;
+
+namespace IssueManager.Utilities
+{
+    public static class RouteHelper
+    {
+        public static Dictionary<string, string> ToRouteValues(this RequestSearchFilters filters, int pageIndex)
+        {
+            var routeValues = new Dictionary<string, string> { { "pageIndex", pageIndex.ToString() } };
+
+            foreach (var prop in typeof(RequestSearchFilters).GetProperties())
+            {
+                var value = prop.GetValue(filters);
+                if (value != null && !string.IsNullOrEmpty(value.ToString()))
+                {
+                    routeValues[prop.Name] = value.ToString()!; 
+                }
+            }
+
+            return routeValues; 
+        }
+    }
+}
