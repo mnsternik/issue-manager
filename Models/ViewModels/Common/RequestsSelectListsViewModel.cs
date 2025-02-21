5da25bb2df9edd7140801c387d52c4c8a77dc5b6
mnsternik <mnsternik@gmail.com>
2025-02-21 18:26:52 +0100

    RequestsSelectListsViewModel created

---
+++ Models/ViewModels/Common/RequestsSelectListsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace IssueManager.Models.ViewModels.Common
+{
+    public class RequestsSelectListsViewModel
+    {
+        public class UsersByTeamItem
+        {
+            public string Id { get; set; } = string.Empty;
+            public string Name { get; set; } = string.Empty;
+        }
+
+        public Dictionary<string, List<UsersByTeamItem>> UsersByTeam { get; set; } = [];
+        public IEnumerable<SelectListItem> TeamSelectOptions { get; set; } = [];
+        public IEnumerable<SelectListItem> UserSelectOptions { get; set; } = [];
+        public IEnumerable<SelectListItem> CategorySelectOptions { get; set; } = [];
+    }
+}
