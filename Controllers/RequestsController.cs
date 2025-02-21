fe056f215926657e477aba01d299d53b2ccf1266
mnsternik <mnsternik@gmail.com>
2025-02-21 18:24:26 +0100

    Database access logic moved to service

--- Controllers/RequestsController.cs
+++ Controllers/RequestsController.cs
@@ -1,41 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using IssueManager.Data;
 using IssueManager.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using IssueManager.Utilities;
-using AutoMapper;
 using IssueManager.Models.ViewModels.Requests;
 using IssueManager.Services.Requests;
 using IssueManager.Exceptions;
 using IssueManager.Services.Files;
-using System.Data;
+using IssueManager.Services.SelectLists;
 
 namespace IssueManager.Controllers
 {
     public class RequestsController : Controller
     {
-        private readonly ApplicationDbContext _context;
         private readonly UserManager<User> _userManager;
-        private readonly IMapper _mapper;
         private readonly IRequestService _requestsService;
         private readonly IFileService _fileService; 
+        private readonly ISelectListService _selectListService;
 
-        const int pageSize = 10;
-
-        public RequestsController(ApplicationDbContext context, UserManager<User> userManager, IMapper mapper, IRequestService requestsService, IFileService fileService)
+        public RequestsController(UserManager<User> userManager, IRequestService requestsService, IFileService fileService, ISelectListService selectListService)
         {
-            _context = context;
             _userManager = userManager;
-            _mapper = mapper;
             _requestsService = requestsService;
             _fileService = fileService;
+            _selectListService = selectListService;
         }
 
         // GET: Requests
-        [HttpGet]
         public async Task<IActionResult> Index(RequestSearchFilters filters, int pageIndex = 1)
         {
             var routeValues = filters.ToRouteValues(pageIndex);
@@ -48,12 +40,10 @@ namespace IssueManager.Controllers
 
             var requestsListViewModel = await _requestsService.GetRequestsAsync(filters, pageIndex);
 
-            PopulateSelectsList();
             return View(requestsListViewModel);
         }
 
         // GET: Requests/Details/5
-        [HttpGet]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -75,8 +65,8 @@ namespace IssueManager.Controllers
         [Authorize(Roles = "User,Admin")]
         public IActionResult Create()
         {
-            PopulateSelectsList();
-            return View(new CreateRequestViewModel());
+            var requestViewModel = _requestsService.GetCreateRequest();
+            return View(requestViewModel);
         }
 
         // POST: Requests/Create
@@ -86,24 +76,25 @@ namespace IssueManager.Controllers
         public async Task<IActionResult> Create(CreateRequestViewModel requestViewModel)
         {
             if (ModelState.IsValid)
-            {           
+            {
                 var currentUserId = _userManager.GetUserId(User)!;
+                int? newRequestId;
 
                 try
                 {
-                    await _requestsService.CreateRequestAsync(requestViewModel, currentUserId);
+                   newRequestId = await _requestsService.CreateRequestAsync(requestViewModel, currentUserId);
                 }
                 catch (InvalidFileTypeException ex)
                 {
                     ModelState.AddModelError("", ex.Message);
-                    PopulateSelectsList();
+                    requestViewModel.SelectLists = _selectListService.PopulateRequestSelectLists(requestViewModel.AssignedUserId, requestViewModel.AssignedTeamId, requestViewModel.CategoryId);
                     return View(requestViewModel);
                 }
-             
-                return RedirectToAction(nameof(Index));
+
+                return RedirectToAction(nameof(Details), new { Id = newRequestId }); 
             }
 
-            PopulateSelectsList();
+            requestViewModel.SelectLists = _selectListService.PopulateRequestSelectLists(requestViewModel.AssignedUserId, requestViewModel.AssignedTeamId, requestViewModel.CategoryId);
             return View(requestViewModel);
         }
 
@@ -128,7 +119,10 @@ namespace IssueManager.Controllers
             {
                 return NotFound(); 
             }
-            //Add DbUpdateConcurrencyException handling
+            catch (DbUpdateConcurrencyException)
+            {
+                throw; // what to do?
+            }
 
             return RedirectToAction(nameof(Details), new { Id = id });
         }
@@ -142,17 +136,13 @@ namespace IssueManager.Controllers
                 return NotFound();
             }
 
-            var requestViewModel = await _mapper
-                .ProjectTo<EditRequestViewModel>(_context.Requests.AsNoTracking())
-                .FirstOrDefaultAsync(r => r.Id == id);
+            var requestViewModel = await _requestsService.GetEditRequestAsync((int)id);
 
             if (requestViewModel == null)
             {
                 return NotFound();
             }
 
-
-            PopulateSelectsList(requestViewModel.AssignedUserId, requestViewModel.AssignedTeamId, requestViewModel.CategoryId);
             return View(requestViewModel);
         }
 
@@ -160,38 +150,29 @@ namespace IssueManager.Controllers
         [HttpPost]
         [Authorize(Roles = "User,Admin")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Status,Priority,CreateDate,UpdateDate,AuthorName,AuthorId,CategoryId,AssignedUserId,AssignedTeamId")] EditRequestViewModel requestViewModel)
+        public async Task<IActionResult> Edit(int id, EditRequestViewModel requestViewModel)
         {
             if (id != requestViewModel.Id)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            try
             {
-                var request = _mapper.Map<Request>(requestViewModel);
-                request.UpdateDate = DateTime.UtcNow;
-
-                try
-                {
-                    _context.Update(request);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!RequestExists(request.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                await _requestsService.EditRequestAsync(requestViewModel); 
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                // TODO
             }
 
-            PopulateSelectsList(requestViewModel.AssignedUserId, requestViewModel.AssignedTeamId, requestViewModel.CategoryId);
+            if (ModelState.IsValid)
+            {
+
+                return RedirectToAction(nameof(Details), new { Id = id });
+            }
+
+            requestViewModel.SelectLists = _selectListService.PopulateRequestSelectLists(requestViewModel.AssignedUserId, requestViewModel.AssignedTeamId, requestViewModel.CategoryId);
             return View(requestViewModel);
         }
 
@@ -200,80 +181,18 @@ namespace IssueManager.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddResponse(int requestId, string responseText)
         {
-            if (!User.Identity!.IsAuthenticated)
+            var currentUserId = _userManager.GetUserId(User)!;
+            
+            try
             {
-                return Unauthorized();
+                await _requestsService.AddResponseAsync(requestId, responseText, currentUserId);
             }
-
-            var response = new RequestResponse
+            catch (Exception) // what kinds of execptions may occur here?
             {
-                RequestId = requestId,
-                ResponseText = responseText,
-                AuthorId = _userManager.GetUserId(User)!,
-                CreateDate = DateTime.UtcNow
-            };
-
-            _context.RequestResponses.Add(response);
-            await _context.SaveChangesAsync();
+                // TODO 
+            }
 
             return RedirectToAction("Edit", new { id = requestId });
-        }
-
-        // GET: Requests/Delete/5
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var request = await _context.Requests
-                .Include(r => r.AssignedTeam)
-                .Include(r => r.AssignedUser)
-                .Include(r => r.Category)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (request == null)
-            {
-                return NotFound();
-            }
-
-            return View(request);
-        }
-
-        // POST: Requests/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [Authorize(Roles = "Admin")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var request = await _context.Requests.FindAsync(id);
-            if (request != null)
-            {
-                _context.Requests.Remove(request);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool RequestExists(int id)
-        {
-            return _context.Requests.Any(e => e.Id == id);
-        }
-
-        private void PopulateSelectsList(string? selectedUserId = null, int? selectedTeamId = null, int? selectedCategoryId = null)
-        {
-            ViewBag.UsersByTeam = _context.Users
-                .GroupBy(u => u.TeamId)
-                .ToDictionary(
-                    g => g.Key.ToString(),
-                    g => g.Select(u => new { id = u.Id, name = u.Name }).ToList()
-                );
-
-            ViewData["TeamSelectOptions"] = new SelectList(_context.Teams, "Id", "Name", selectedTeamId);
-            ViewData["UserSelectOptions"] = new SelectList(_context.Users, "Id", "Name", selectedUserId);
-            ViewData["CategorySelectOptions"] = new SelectList(_context.Categories, "Id", "Name", selectedCategoryId);
         }
     }
 }
