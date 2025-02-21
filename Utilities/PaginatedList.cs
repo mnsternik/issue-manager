86cbc4637d0b8f5f22e01947979fd394df586b85
mnsternik <mnsternik@gmail.com>
2025-02-21 18:25:53 +0100

    Const pageSize added

--- Utilities/PaginatedList.cs
+++ Utilities/PaginatedList.cs
@@ -7,6 +7,8 @@ namespace IssueManager.Utilities
         public int PageIndex { get; private set; }
         public int TotalPages { get; private set; }
 
+        private const int pageSize = 10;
+
         public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
         {
             PageIndex = pageIndex;
@@ -19,7 +21,7 @@ namespace IssueManager.Utilities
 
         public bool HasNextPage => PageIndex < TotalPages;
 
-        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex)
         {
             var count = await source.CountAsync();
             var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
