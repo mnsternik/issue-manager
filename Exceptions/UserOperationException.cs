d7ccad645e272a93862075da88bfb8a2df4272f3
mnsternik <mnsternik@gmail.com>
2025-02-25 23:47:04 +0100

    UserOperationException created

---
+++ Exceptions/UserOperationException.cs
@@ -0,0 +1,13 @@
+﻿namespace IssueManager.Exceptions
+{
+    public class UserOperationException : Exception
+    {
+        public List<string> Errors { get; }
+
+        public UserOperationException(List<string> errors)
+            : base($"User operation failed: {string.Join("; ", errors)}")
+        {
+            Errors = errors;
+        }
+    }
+}
