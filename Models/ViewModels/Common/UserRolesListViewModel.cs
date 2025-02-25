d02f5629ab9044730235895b497dc0fad18ebdf2
mnsternik <mnsternik@gmail.com>
2025-02-25 23:48:27 +0100

    ViewModels for user related select lists created

---
+++ Models/ViewModels/Common/UserRolesListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueManager.Models.ViewModels.Common
+{
+    public class ExistingUserRolesListViewModel
+    {
+        public IList<string> CurrentRoles { get; set; } = [];
+        public IList<string> AvailableRoles { get; set; } = [];
+        public IList<string> SelectedRoles { get; set; } = [];
+    }
+
+    public class NewUserRolesListViewModel
+    {
+        public IList<string> AvailableRoles { get; set; } = [];
+
+        [Required(ErrorMessage = "At least one role must be selected.")]
+        [MinLength(1, ErrorMessage = "At least one role must be selected.")]
+        public IList<string> SelectedRoles { get; set; } = [];
+    }
+}
---
+++ Models/ViewModels/Common/UserSelectListsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace IssueManager.Models.ViewModels.Common
+{
+    public class UserSelectListsViewModel
+    {
+        public IEnumerable<SelectListItem> TeamSelectOptions { get; set; } = [];
+    }
+}
