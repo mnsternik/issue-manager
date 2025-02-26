dc1d60057a61a4a29ca00cd147574e662046db6d
mnsternik <mnsternik@gmail.com>
2025-02-26 23:44:37 +0100

    Db access logic moved to categories service

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,78 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using IssueManager.Data;
 using IssueManager.Models;
-using IssueManager.Utilities;
-using AutoMapper;
 using IssueManager.Models.ViewModels.Categories;
+using IssueManager.Services.Categories;
+using IssueManager.Exceptions;
+using Microsoft.AspNetCore.Authorization;
 
 namespace IssueManager.Controllers
 {
     public class CategoriesController : Controller
     {
-        private readonly ApplicationDbContext _context;
-        private readonly IMapper _mapper;
+        private readonly ICategoryService _categoryService; 
 
-        public CategoriesController(ApplicationDbContext context, IMapper mapper)
+        public CategoriesController(ICategoryService categoryService)
         {
-            _context = context;
-            _mapper = mapper;
+            _categoryService = categoryService; 
         }
 
         // GET: Categories
         public async Task<IActionResult> Index(string search, int pageIndex = 1)
         {
-            const int pageSize = 10;
-
-            IQueryable<Category> query = _context.Categories;
-
-            if (!string.IsNullOrEmpty(search))
-            {
-                query = query.Where(t => t.Name.ToLower().Contains(search));
-            }
-
-            IQueryable<CategoriesListItemViewModel> mappedQuery = _mapper.ProjectTo<CategoriesListItemViewModel>(query);
-
-            var paginatedCategories = await PaginatedList<CategoriesListItemViewModel>.CreateAsync(mappedQuery, pageIndex, pageSize);
-
-            var categoriesViewModel = new CategoriesListViewModel
-            {
-                Categories = paginatedCategories,
-                SearchString = search
-            };
-
+            var categoriesViewModel = await _categoryService.GetCategoriesAsync(search, pageIndex); 
             return View(categoriesViewModel);
         }
 
         // GET: Categories/Create
+        [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Teams/Create
+        // POST: Categories/Create
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] CreateCategoryViewModel categoryViewModel)
+        public async Task<IActionResult> Create(CreateCategoryViewModel categoryViewModel)
         {
             if (ModelState.IsValid)
             {
-                if (CategoryNameExists(categoryViewModel.Name))
+                try
+                {
+                    await _categoryService.CreateCategoryAsync(categoryViewModel);
+                }
+                catch (NameAlreadyExistsException)
                 {
                     ModelState.AddModelError("Name", "Category with this name already exists");
-                    return View(categoryViewModel); 
+                    return View(categoryViewModel);
                 }
-
-                var category = _mapper.Map<Category>(categoryViewModel); 
-
-                _context.Add(category);
-                await _context.SaveChangesAsync();
+       
                 return RedirectToAction(nameof(Index));
             }
+
             return View(categoryViewModel);
         }
 
         // GET: Categories/Edit/5
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -80,18 +64,21 @@ namespace IssueManager.Controllers
                 return NotFound();
             }
 
-            var category = await _context.Categories.FindAsync(id);
+            var category = await _categoryService.GetCategoryAsync((int)id);
+
             if (category == null)
             {
                 return NotFound();
             }
+
             return View(category);
         }
 
         // POST: Categories/Edit/5
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Category category)
+        public async Task<IActionResult> Edit(int id, Category category)
         {
             if (id != category.Id)
             {
@@ -100,34 +87,28 @@ namespace IssueManager.Controllers
 
             if (ModelState.IsValid)
             {
-                if (CategoryNameExists(category.Name))
+                try
+                {
+                    await _categoryService.EditCategoryAsync(category);
+                }
+                catch (NameAlreadyExistsException)
                 {
                     ModelState.AddModelError("Name", "Category with this name already exists");
                     return View(category);
                 }
-
-                try
-                {
-                    _context.Update(category);
-                    await _context.SaveChangesAsync();
-                }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!CategoryIdExists(category.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    // TODO 
                 }
+
                 return RedirectToAction(nameof(Index));
             }
+
             return View(category);
         }
 
         // GET: Categories/Delete/5
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -135,8 +116,8 @@ namespace IssueManager.Controllers
                 return NotFound();
             }
 
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var category = await _categoryService.GetCategoryAsync((int)id);
+
             if (category == null)
             {
                 return NotFound();
@@ -147,27 +128,20 @@ namespace IssueManager.Controllers
 
         // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
+        [Authorize(Roles = "Admin")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var category = await _context.Categories.FindAsync(id);
-            if (category != null)
+            try
             {
-                _context.Categories.Remove(category);
+                await _categoryService.DeleteCategoryAsync(id);
+            }
+            catch
+            {
+                // TODO
             }
 
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool CategoryIdExists(int id)
-        {
-            return _context.Categories.Any(e => e.Id == id);
-        }
-
-        private bool CategoryNameExists(string name)
-        {
-            return _context.Categories.Any(c => c.Name == name); 
         }
     }
 }
