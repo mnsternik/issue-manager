98891c7b5e6c603a984b5cba59ac1d830d3463e9
mnsternik <mnsternik@gmail.com>
2025-03-03 23:30:03 +0100

    Seeding Team data moved to DataSeeder class

--- IssueManager/Data/ApplicationDbContext.cs
+++ IssueManager/Data/ApplicationDbContext.cs
@@ -15,45 +15,29 @@ namespace IssueManager.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            // Relacja do użytkownika (AssignedToUser)
             modelBuilder.Entity<Request>()
                 .HasOne(r => r.AssignedUser)
                 .WithMany()
                 .HasForeignKey(r => r.AssignedUserId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            // Relacja do zespołu (AssignedToTeam)
             modelBuilder.Entity<Request>()
                 .HasOne(r => r.AssignedTeam)
                 .WithMany()
                 .HasForeignKey(r => r.AssignedTeamId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            // Relacja do odpowiedzi do złoszenia (Responses)
             modelBuilder.Entity<Request>()
                 .HasMany(r => r.Responses)
                 .WithOne(rr => rr.Request)
                 .HasForeignKey(rr => rr.RequestId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            // Realcja odpowiedzi zgłoszenia do jej autora 
             modelBuilder.Entity<RequestResponse>()
                 .HasOne(rr => rr.Author)
                 .WithMany()
                 .HasForeignKey(rr => rr.AuthorId)
                 .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Team>().HasData(new Team
-            {
-                Id = 2,
-                Name = "Helpdesk"
-            });
-
-            modelBuilder.Entity<Category>().HasData(new Category
-            {
-                Id = 2,
-                Name = "Applications"
-            });
         }
 
         public DbSet<Request> Requests { get; set; }
