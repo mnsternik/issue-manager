91250c7463b16906a7ffb5c57791ac6244200562
mnsternik <mnsternik@gmail.com>
2025-03-21 12:27:45 +0100

    EntityNotFoundException created

---
+++ IssueManager/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace IssueManager.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string entityName, object key)
+            : base($"Entity '{entityName}' with key '{key}' was not found.") { }
+
+        public EntityNotFoundException(string message) : base(message) { }
+
+        public EntityNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
